4ab874b8656c4e094c3a5a418e8e6c79568a6b07
Mike <michaeljhernandez90@gmail.com>
2019-08-15 09:57:01 -0600

    work update
    gridcomponent now populates json date; now need to format data

--- src/components/GridComponent.vue
+++ src/components/GridComponent.vue
@@ -1,54 +1,19 @@
 <template>
-    <div class="table-responsive">
-        <table class="table-hover" v-if="data">
-            <thead>
-                <tr>
-                    <th>Business</th>
-                    <th>Founder</th>
-
-                    <!-- and so on -->
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(item, idx) in archivelist" v-bind:key="item.id">
-                    <td>{{ item.businessName }}></td>
-                    <td>{{ item.founderName }}></td>
-
-                    <!-- and so on -->
-                </tr>
-            </tbody>
-        </table>
+    <div>
+        <div v-for="data in myJson" :key="data.id">{{data}}</div>
     </div>
 </template>
 
 
+
+
 <script>
-export default {
-  name: 'GridComponent',
-  data() {
-    return {
-        data: null
-    };
-},
-
-mounted() {
-    this.getData();
-
-},
-
-methods: {
-    getData() {
-        // vue-resource example
-        this.$http.get('data.json', {responseType: 'json'}).then(response => {
-            return response.json();
-        }).then(jsonData => {
-            this.data = JSON.parse(jsonData);
-        }).catch(e => {
-            console.log('Error', e);
-        });
-    }
-}
-
-  }
-
+      import json from '@/data/data.json'
+      export default{
+          data: function (){
+              return{
+                  myJson: json
+              }
+          }
+      }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
+import axios from 'axios'
+
+axios.defaults.baseURL = "https://api.example.com"
 
 Vue.config.productionTip = false
 
