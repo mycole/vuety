9b0a0131206497bdf1aa9954cc66a0fef160437a
mycoal <michaeljhernandez90@gmail.com>
2019-08-22 16:26:37 -0600

    home update

---
+++ src/views/Test7.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    {{ msg }}
+<GridComponentDataFour></GridComponentDataFour>
+
+
+</div>
+</template>
+
+  <script>
+  import GridComponentDataFour from '@/components/GridComponentDataFour.vue'
+
+
+  export default {
+    name: 'Test7',
+    data() {
+    return {
+
+
+      msg: 'das Haus der Prüfung sept'
+    }
+  },
+  components: {
+    GridComponentDataFour
+
+  }
+    }
+
+</script>
+
+<style scoped>
+
+
+
+</style>
+
+<!-- https://012.vuejs.org/guide/forms.html
+https://vuejs.org/v2/guide/
+https://vuejs.org/v2/guide/forms.html
+https://vuejs.org/v2/guide/forms.html#Basic-Usage
+-->
