c366c7eb1a23ca6313a43a03103ce84661dab611
Mike <michaeljhernandez90@gmail.com>
2019-08-22 12:19:21 -0600

    fixed view 6
    check box values sum

--- src/components/GridComponentDataThree.vue
+++ src/components/GridComponentDataThree.vue
@@ -1,86 +1,92 @@
 <template>
-    <div>
-
-
-      <table>
-        <thead class="thead-dark">
-                      <tr>
-                          <th>
-                            <input type="checkbox" v-model="selectAll" @click="selectall">
-                            <i class="form-icon"></i>
-                          </th>
-                          <th>id#</th>
-                          <th>Full Name</th>
-                          <th>Email</th>
-                          <th>Color</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      <tr v-for="user in list"
-                      v-bind:key="user.id"
-                      >
-                        <input type="checkbox" v-model="checked" :value="user.id" @click="selectid">
-                          <td>{{user.id}}</td>
-                          <td>{{user.name}}</td>
-                          <td>{{user.email}}</td>
-                          <td>{{user.color}}</td>
-                      </tr>
-                      <tr>{{checked}}</tr>
-                  </tbody>
+  <div>
+    <table>
+      <thead class="thead-dark">
+        <tr>
+          <th>
+            <input type="checkbox" v-model="selectAll" @click="selectall">
+            <i class="form-icon"></i>
+          </th>
+          <th>id#</th>
+          <th>Full Name</th>
+          <th>Email</th>
+          <th>Color</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in list" v-bind:key="user.id">
+          <input type="checkbox" v-model="checked" :value="user.id" @click="selectid">
+          <td>{{user.id}}</td>
+          <td>{{user.name}}</td>
+          <td>{{user.email}}</td>
+          <td>{{user.color}}</td>
+          <td>{{user.point}}</td>
+        </tr>
+        <tr>
+          <strong>Total:</strong>
+          <span class="content">{{ totalMarks }}</span>
+        </tr>
+        <tr>{{checked}}</tr>
+      </tbody>
     </table>
-    </div>
+  </div>
 </template>
 
 
 
 
 <script>
-import users from "@/data/data2.json"
+import users from "@/data/data2.json";
 
-      export default{
-        name: "GridComponentDataTwo",
-        data: function() {
-          return {
-           users,
-           checked: [],
-           selectAll: false,
-           selectID: false,
-           userIDs: []
-          }
-        },
-        methods: {
-                function() {
-                    json => this.users=json;
-                            },
+export default {
+  name: "GridComponentDataTwo",
+  data: function() {
+    return {
+      users,
+      checked: [],
+      selectAll: false,
+      selectID: false,
+      userIDs: []
+    };
+  },
+  methods: {
+    function() {
+      json => (this.users = json);
+    },
 
-              selectall() {
-                        this.checked = [];
-                        if (!this.selectAll) {
-                          for (let user in this.list) {
-                            this.checked.push(this.list[user].id);
-                          }
-                        }
-},
-selectid() {
-    this.userIDs = [];
-    if (this.selectID) {
-      for (users in this.users) {
-        this.userIds.push(this.users[users].id);
+    selectall() {
+      this.checked = [];
+      if (!this.selectAll) {
+        for (let user in this.list) {
+          this.checked.push(this.list[user].id);
+        }
+      }
+    },
+    selectid() {
+      this.userIDs = [];
+      if (this.selectID) {
+        for (users in this.users) {
+          this.userIds.push(this.users[users].id);
+        }
       }
     }
-
-
-
-}
-},
-computed: {
-  list: function() {
-    return this.users.filter(function(user){
-      return user.color === 'red'
-    })
+  },
+  computed: {
+    list: function() {
+      return this.users.filter(function(user) {
+        return user.color === "red";
+        // return user.color !== "green";
+      });
+    },
+    totalMarks: function() {
+      let total = 0;
+      for (let i = 0; i < this.checked.length; i++) {
+        total += parseInt(this.list[i].point);
+      }
+      return total;
+    }
   }
-}
-}
+};
 
 // WORKING: https://stackoverflow.com/questions/56628157/vue-populate-table-with-axios
 //https://stackoverflow.com/questions/45565349/how-to-acces-external-json-file-objects-in-vue-js-app/45566350#45566350
